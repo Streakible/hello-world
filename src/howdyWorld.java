37e225a8d276c99fd648a6e605361146d92d73b6
Andrew McGran <ntb16156@uni.strath.ac.uk>
2017-02-10 22:16:23 +0000

    testy time

--- src/howdyWorld.java
+++ src/howdyWorld.java
@@ -4,6 +4,7 @@ public class howdyWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("howdy yall");
+		System.out.println("wowee.");
 	}
 
 }
